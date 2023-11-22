74050f0045adf18ababc005304d17324dfe5a505
VladimirPichuev <rine-s@ya.ru>
2023-11-22 11:05:35 +0800

    save secrets

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,17 +7,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$' 
+#SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$' 
 
-DEBUG = True 
+#DEBUG = True 
 
-ALLOWED_HOSTS = ['rinens.sytes.net', '127.0.0.1', 'localhost', '158.160.0.130'] 
+#ALLOWED_HOSTS = ['rinens.sytes.net', '127.0.0.1', 'localhost', '158.160.0.130'] 
 
-#SECRET_KEY = os.getenv('SECRET_KEY', 'django')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
-#DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG') == 'True'
 
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['rinens.sytes.net', '127.0.0.1', 'localhost', '158.160.0.130'])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['rinens.sytes.net', '127.0.0.1', 'localhost', '158.160.0.130'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
